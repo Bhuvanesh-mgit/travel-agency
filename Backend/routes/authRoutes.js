3c938ca15bd69ff631f2f3a8ca9790641e37699d
Bhuvanesh-mgit <bhuvanesh.m444@gmail.com>
2026-09-02 10:32:25 +0530

    auth commit

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -8,7 +8,7 @@ import {
   updateUser,
   deleteUser,
 } from '../controllers/authController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect } from '../MiddleWare/authMiddleware.js';
 
 const router = express.Router();
 
