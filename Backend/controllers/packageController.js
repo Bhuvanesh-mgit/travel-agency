7ec245fbb978899981a2edb9a9b70188161289cd
Bhuvanesh-mgit <bhuvanesh.m444@gmail.com>
2026-09-02 10:59:08 +0530

    Force add multer.js middleware for Render

--- Backend/controllers/packageController.js
+++ Backend/controllers/packageController.js
@@ -1,5 +1,5 @@
 import Package from '../models/packageModel.js';
-import { cloudinary } from '../middleware/multer.js';
+import {cloudinary } from '../middleware/multer.js';
 import fs from 'fs';
 import path from 'path';
 
